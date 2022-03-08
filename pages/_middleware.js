a87f9b3ed2e8d2c0733828f3db4c7132e96aac4a
Brian <brian@briankiernan.com>
2022-03-07 19:36:10 -0500

    update middleware

--- pages/_middleware.js
+++ pages/_middleware.js
@@ -12,10 +12,9 @@ export default function middleware(req) {
 
   const currentHost =
     process.env.NODE_ENV === 'production' && process.env.VERCEL === '1'
-      ? hostname
-          .replace(`.myhomecomp.vercel.app`, '') // you have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
-          .replace(`.platformize.vercel.app`, '') // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
-      : // in this case, our team slug is "platformize", thus *.platformize.vercel.app works
+      ? hostname.replace(`.myhomecomp.com`, '') // you have to replace ".vercel.pub" with your own domain if you deploy this example under your domain.
+      : // .replace(`.platformize.vercel.app`, '') // you can use wildcard subdomains on .vercel.app links that are associated with your Vercel team slug
+        // in this case, our team slug is "platformize", thus *.platformize.vercel.app works
         hostname.replace(`.localhost:3000`, '');
 
   if (pathname.startsWith(`/_sites`)) {
